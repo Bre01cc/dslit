e85d42a94a69115bf16f370f5f57f0e4559d109e
Bre01cc <boliveiraassisreis@gmail.com>
2025-05-24 23:48:33 -0300

    inicio do projeto

---
+++ src/main/java/com/devBreno/dslist097/Dslist097Application.java
@@ -0,0 +1,13 @@
+package com.devBreno.dslist097;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist097Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist097Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devBreno/dslist097/Dslist097ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devBreno.dslist097;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist097ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
